06cc08adce2b3903c0be434663d6a12c9631dda3
vanes3355 <100181102+vanes3355@users.noreply.github.com>
2022-05-31 00:47:19 +0700

    Add files via upload

---
+++ praktikum 2/bujursangkar.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int sisi;
+
+    printf("Masukkan sisi: ");
+    scanf("%d", &sisi);
+    
+    printf("%d", sisi*sisi);
+
+    return 0;
+}
